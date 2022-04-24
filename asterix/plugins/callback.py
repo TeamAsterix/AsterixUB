51f21a89acc032152a5d53b7207d3beec42ed04d
Ryo <89829215+Ryoishin@users.noreply.github.com>
2022-04-25 00:08:38 +0530

    Real Name

--- asterix/plugins/callback.py
+++ asterix/plugins/callback.py
@@ -105,7 +105,7 @@ async def _stats(_, cb):
 @app.alert_user
 async def _about(_, cb):
 	await cb.edit_message_text(
-		text=f"**Dex:** About\n\n**Location:** /home/about\n\n**[ Personal Info ]:**\n\n**Age:** {app.assistant_age}\n**Name:** {app.assistant_name}\n**Gender:** {app.assistant_gender}\n\n**[ Versions ]:**\n\n**Python:** {app.python_version}\n**Pyrogram:** {app.pyrogram_version}\n**Assistant:**  {app.assistant_version}\n\n**[ About ]:**\n\nI am Nora made by ࿇•ẞᗴᗩSԵ•࿇\nI am your friendly assistant, i will help you as much as you need. You can ask me for any help related to your userbot. If you have any suggestions or you're facing any problems which are related to asterixuserbot then just ask in [support group](https://t.me/asterixuserbot_support), ",
+		text=f"**Dex:** About\n\n**Location:** /home/about\n\n**[ Personal Info ]:**\n\n**Age:** {app.assistant_age}\n**Name:** {app.assistant_name}\n**Gender:** {app.assistant_gender}\n\n**[ Versions ]:**\n\n**Python:** {app.python_version}\n**Pyrogram:** {app.pyrogram_version}\n**Assistant:**  {app.assistant_version}\n\n**[ About ]:**\n\nI am Khushi made by Ryoishin\nI am your friendly assistant, i will help you as much as you need. You can ask me for any help related to your userbot. If you have any suggestions or you're facing any problems which are related to asterixuserbot then just ask in [support group](https://t.me/asterixuserbot_support), ",
 		reply_markup=InlineKeyboardMarkup([home_back]),
 	)
 
