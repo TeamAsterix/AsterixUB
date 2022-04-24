3888046885849efb4fb61ed61b3b0065dff9283c
Ryo <89829215+Ryoishin@users.noreply.github.com>
2022-04-24 13:37:08 +0530

    .

---
+++ asterix/helpers/containers/variables.py
@@ -0,0 +1,14 @@
+class Variables(object):
+	# lower
+	message_ids = {}
+
+	# assistant
+	assistant_name = "Kushina"
+	assistant_age = "2"
+	assistant_gender = "Female"
+
+	# userbot
+	userbot_name = "Asterix"
+
+	# photo
+	PIC = "https://telegra.ph/file/38eec8a079706b8c19eae.mp4"
