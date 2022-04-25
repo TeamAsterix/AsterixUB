29a71866f69294a7d77123d47f1aabccc58c7782
Chamindu Denuwan <90548223+ImDenuwan@users.noreply.github.com>
2022-04-25 08:32:35 +0530

    fix

--- session.py
+++ session.py
@@ -38,7 +38,7 @@ API_HASH = input("\nEnter API HASH: ")
 
 
 # create a new pyrogram session
-with Client(":memory:", api_id=int(API_ID), api_hash=API_HASH) as app:
+with Client("in_memory=True", api_id=int(API_ID), api_hash=API_HASH) as app:
   app.send_message("me", f"This Is Your Asterix Userbot • [ `SESSION` ]\n\n```{app.export_session_string()}```\n\n⚠️• Don't share this with anyone !!\n\nCreate Again • [ Press Here](https://replit.com/@TeamAsterix/AsterixUB)", disable_web_page_preview=True,) 
   print("Your String Session Is Successfully Saved In Telegram Saved (Cloud) Messages !! Don't Share It With Anyone!! Anyone having your session can use (hack) your telegram account !")
   
