9bc6d0220d2bfff4a585db4d26bbb0390fba1bcd
Ryo <89829215+Ryoishin@users.noreply.github.com>
2022-07-18 19:54:14 +0530

    Try

---
+++ Public/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+/** module tdlight.java {
+	requires tdlight.api;
+	requires org.reactivestreams;
+	requires org.slf4j;
+	requires it.unimi.dsi.fastutil;
+	requires com.google.zxing;
+	exports it.tdlight.tdlight;
+	exports it.tdlight.tdnative;
+	exports it.tdlight.tdlib;
+	exports it.tdlight.common;
+	exports it.tdlight.common.utils;
+	exports it.tdlight.common.internal;
+	exports it.tdlight.client;
+}/*
